d3c311b2d71c0267e36c3328238e5e6fbf09a236
Slava Petrov <slavik--petrov@yandex.ru>
2016-03-03 14:53:31 +0300

    Update RNA-transcription usins regexps

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -2,17 +2,8 @@ class Complement
   VERSION = 3
   def self.of_dna(*args)
     dna = args[0]    
-    rna = ""
-    complements = {'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}
+    raise ArgumentError if dna =~ /[^GCTA]/
 
-    dna.each_char do |c| 
-      if complements[c]
-        rna += complements[c]
-      else
-        raise ArgumentError 
-      end
-    end
-
-    rna
+    dna.gsub(/./, 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U')
   end
 end
