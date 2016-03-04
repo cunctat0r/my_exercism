6ff368d4ef23adefa4e02fbc2f3d955f40345f67
Slava Petrov <slavik--petrov@yandex.ru>
2016-03-04 15:36:28 +0300

    Food-chain checked by rubocop

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -1,53 +1,60 @@
+# FoodChain exercism.io exercise
 class FoodChain
   VERSION = 2
   def self.song
     @persons = [
-      {:person => 'fly', :remark => ""},
-      {:person => 'spider', :remark => "It wriggled and jiggled and tickled inside her."},
-      {:person => 'bird', :remark => "How absurd to swallow a bird!"},
-      {:person => 'cat', :remark => "Imagine that, to swallow a cat!"},
-      {:person => 'dog', :remark => "What a hog, to swallow a dog!"},
-      {:person => 'goat', :remark => "Just opened her throat and swallowed a goat!"},
-      {:person => 'cow', :remark => "I don't know how she swallowed a cow!"},
-      {:person => 'horse', :remark => ""}
+      { person: 'fly',
+        remark: '' },
+      { person: 'spider',
+        remark: 'It wriggled and jiggled and tickled inside her.' },
+      { person: 'bird',
+        remark: 'How absurd to swallow a bird!' },
+      { person: 'cat',
+        remark: 'Imagine that, to swallow a cat!' },
+      { person: 'dog',
+        remark: 'What a hog, to swallow a dog!' },
+      { person: 'goat',
+        remark: 'Just opened her throat and swallowed a goat!' },
+      { person: 'cow',
+        remark: "I don't know how she swallowed a cow!" },
+      { person: 'horse',
+        remark: '' }
     ]
 
-    lyrics = ""
+    lyrics = ''
 
     0.upto(7) do |iteration|
-      lyrics +=  "#{generate_part(iteration)}\n"      
+      lyrics +=  "#{generate_part(iteration)}\n"
     end
     lyrics
-
   end
 
-  private
-
   def self.generate_part(person_num)
-    lyrics = ""
+    lyrics = ''
     if person_num == 0
-      # fly
       lyrics = "I know an old lady who swallowed a fly.\n"
     elsif person_num == 7
-      # horse
-      lyrics = "I know an old lady who swallowed a horse.\nShe's dead, of course!"
+      lyrics = 'I know an old lady who swallowed a horse.'\
+        "\nShe's dead, of course!"
       return lyrics
     else
-      lyrics += "I know an old lady who swallowed a #{@persons[person_num][:person]}.\n"
+      lyrics += 'I know an old lady who swallowed a'\
+        " #{@persons[person_num][:person]}.\n"
       lyrics += "#{@persons[person_num][:remark]}\n"
-      person_num.downto(1) do |num|        
-        if @persons[num][:person] == 'bird' 
-          modified_remark = @persons[num-1][:remark]
+      person_num.downto(1) do |num|
+        if @persons[num][:person] == 'bird'
+          modified_remark = @persons[num - 1][:remark]
           modified_remark.gsub!(/It /, '')
-          lyrics += "She swallowed the #{@persons[num][:person]} to catch the #{@persons[num-1][:person]} that #{modified_remark}\n"
+          lyrics += "She swallowed the #{@persons[num][:person]}"\
+            " to catch the #{@persons[num - 1][:person]}"\
+            " that #{modified_remark}\n"
         else
-          lyrics += "She swallowed the #{@persons[num][:person]} to catch the #{@persons[num - 1][:person]}.\n"
+          lyrics += "She swallowed the #{@persons[num][:person]}"\
+            " to catch the #{@persons[num - 1][:person]}.\n"
         end
-      end      
+      end
     end
-    lyrics +=  "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+    lyrics += "I don't know why she swallowed the fly. Perhaps she'll die.\n"
     lyrics
   end
 end
-
-FoodChain.song
