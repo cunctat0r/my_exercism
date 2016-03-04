96684af1ddccd508066aac6c7b5fc3e6b10f877c
Slava Petrov <slavik--petrov@yandex.ru>
2016-03-04 15:17:06 +0300

    Sieve checked by rubocop

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,3 +1,4 @@
+# Sieve
 class Sieve
   def initialize(initial)
     @initial = initial
@@ -6,8 +7,9 @@ class Sieve
   def primes
     init_sieve = (2..@initial).to_a
     init_sieve.each do |element|
-      init_sieve.delete_if{|number| (number % element == 0) && (number != element)}
-    end    
+      init_sieve.delete_if do |number|
+        (number % element == 0) && (number != element)
+      end
+    end
   end
-
 end
