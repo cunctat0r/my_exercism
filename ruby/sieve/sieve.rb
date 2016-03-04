3abe3a9439071672aa85eebb8948cac9b54cfe16
Slava Petrov <slavik--petrov@yandex.ru>
2016-03-04 14:55:04 +0300

    Sieve

---
+++ ruby/sieve/sieve.rb
@@ -0,0 +1,13 @@
+class Sieve
+  def initialize(initial)
+    @initial = initial
+  end
+
+  def primes
+    init_sieve = (2..@initial).to_a
+    init_sieve.each do |element|
+      init_sieve.delete_if{|number| (number % element == 0) && (number != element)}
+    end    
+  end
+
+end
