c0b7033f29eb2d710d4e9a04ad97eab5e2b752e1
Slava Petrov <slavik--petrov@yandex.ru>
2016-03-09 13:47:45 +0300

    Phone number more refactored solution

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -8,9 +8,8 @@ class PhoneNumber
     bad_number = '0000000000'
     return bad_number if @num =~ /\D+/
     return bad_number if @num.length < 10 || @num.length > 11
-    if @num.length == 11
-      @num = (@num[0] == '1') ? @num[1..@num.length] : bad_number
-    end
+    return bad_number if @num.length == 11 && !@num.start_with?('1')
+    @num = @num[1..@num.length] if @num.length == 11
     @num
   end
 
