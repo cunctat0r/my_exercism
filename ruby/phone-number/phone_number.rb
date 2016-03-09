927ca3d3915bd5301b6ce4e7ad5d333143a85e6c
Slava Petrov <slavik--petrov@yandex.ru>
2016-03-09 13:38:27 +0300

    Phone number refactoredsolution

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -5,10 +5,11 @@ class PhoneNumber
   end
 
   def number
-    return '0000000000' if @num =~ /\D+/
-    return '0000000000' if @num.length < 10 || @num.length > 11
+    bad_number = '0000000000'
+    return bad_number if @num =~ /\D+/
+    return bad_number if @num.length < 10 || @num.length > 11
     if @num.length == 11
-      @num = (@num[0] == '1') ? @num[1..@num.length] : '0000000000'
+      @num = (@num[0] == '1') ? @num[1..@num.length] : bad_number
     end
     @num
   end
