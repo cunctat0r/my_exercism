bfa13b2fec96ee75fec512e0f4b1f58026bda872
Slava Petrov <slavik--petrov@yandex.ru>
2016-04-01 09:49:18 +0300

    Pythagorean triplet better solution

--- ruby/pythagorean-triplet/pythagorean_triplet.rb
+++ ruby/pythagorean-triplet/pythagorean_triplet.rb
@@ -1,37 +1,36 @@
 #
 class Triplet
   def initialize(a, b, c)
-    @a = a
-    @b = b
-    @c = c
+    @triplet = [a, b, c]
   end
 
   def sum
-    @a + @b + @c
+    @triplet.reduce(:+)
   end
 
   def product
-    @a * @b * @c
+    @triplet.reduce(:*)
   end
 
   def pythagorean?
-    @a**2 + @b**2 == @c**2
+    a, b, c = @triplet
+    a**2 + b**2 == c**2
   end
 
   def self.where(factor)
     min_factor = factor[:min_factor] || 1
     max_factor = factor[:max_factor]
-    (min_factor..max_factor).each_with_object([]) do |a, result|
-      a.upto(max_factor) do |b|
-        b.upto(max_factor) do |c|
-          t = Triplet.new(a, b, c)
-          next unless t.pythagorean?
-          if factor[:sum]
-            result << t if t.sum == factor[:sum]
-          else
-            result << t
-          end
-        end
+    (min_factor..max_factor)
+      .to_a
+      .combination(3)
+      .each_with_object([]) do |combination, result|
+      a, b, c = combination
+      t = Triplet.new(a, b, c)
+      next unless t.pythagorean?
+      if factor[:sum]
+        result << t if t.sum == factor[:sum]
+      else
+        result << t
       end
     end
   end
