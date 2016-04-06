1663c62dc7bb54189243762b7e398b4b301efde3
Slava Petrov <slavik--petrov@yandex.ru>
2016-04-06 14:58:36 +0300

    Hamming more correct solution

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,15 +1,12 @@
+#
 class Hamming
   VERSION = 1
   def self.compute(*args)
     dna1 = args[0]
     dna2 = args[1]
-    
     raise ArgumentError if dna1.length != dna2.length
-
-    distance = 0
-    0.upto(dna1.length) do |item|
-      distance += 1 if dna1[item] != dna2[item]
+    (0..dna1.length).to_a.inject(0) do |distance, item|
+      dna1[item] == dna2[item] ? distance : distance + 1
     end
-    distance
   end
 end
