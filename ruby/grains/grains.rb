31d58d70c59f8686719fabd9ec4542e25825461d
Slava Petrov <slavik--petrov@yandex.ru>
2016-04-07 09:24:40 +0300

    Grains updated solution

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -4,7 +4,7 @@ class Grains
     2**(square_number - 1)
   end
 
-  def self.total
+  def self.total    
     (1..64).to_a.each.inject(0) do |total, square_number|
       total + square(square_number)
     end
