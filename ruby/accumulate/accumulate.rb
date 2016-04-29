43fc5b198ec55f803b6018976dc359b46fdc4625
Slava Petrov <slavik--petrov@yandex.ru>
2016-04-29 15:00:27 +0300

    Fix error in Accumulate

--- ruby/accumulate/accumulate.rb
+++ ruby/accumulate/accumulate.rb
@@ -1,7 +1,7 @@
 #
 module Accumulate
   def accumulate
-    return enum_for(:each) unless block_given?
+    return enum_for(:accumulate) unless block_given?
     each_with_object([]) { |item, arr| arr << yield(item) }
   end
 end
